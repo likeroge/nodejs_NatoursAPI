302a66daed606ab90bbc11dd07cd49eda6f4a96d
Egor <likeroge@gmail.com>
2019-10-23 22:48:55 +0400

    Added monogodb connection via mongoose

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({path: './config.env'});
 const app = require('./app');
 
-console.log(process.env);
+dotenv.config({path: './config.env'});
+
+const db = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+
+mongoose.connect(db, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(()=>{
+  console.log('DB connection successful!');
+});
 
 const port = process.env.PORT|| 3000;
 app.listen(port, () => {
